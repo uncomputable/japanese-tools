928fdfe9741c3004224cbb0d4ace673dc072b145
Christian Lewe <ch_lewe@agdsn.me>
2024-01-24 23:40:53 +0100

    BCCWJ: Add

---
+++ bccwj.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from occurrence import OccurrenceBag, OccurrenceReader
+
+def read_bag(path_suw: str, path_luw: Optional[str] = None) -> OccurrenceBag:
+    bag = OccurrenceReader() \
+        .with_path(path_suw) \
+        .with_separator("\t") \
+        .with_text_index(2) \
+        .with_reading_index(1) \
+        .with_count_index(6) \
+        .read()
+    if path_luw:
+        other = OccurrenceReader() \
+            .with_path(path_luw) \
+            .with_separator("\t") \
+            .with_skip_lines(1) \
+            .with_text_index(2) \
+            .with_reading_index(1) \
+            .with_count_index(6) \
+            .read()
+        bag.extend_conservative(other)
+    return bag
