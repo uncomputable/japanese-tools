da8cf012bf86f61442544f8330929dbf7cd54228
Christian Lewe <ch_lewe@agdsn.me>
2024-01-27 19:19:06 +0100

    Nix: Add description

--- shell.nix
+++ shell.nix
@@ -1,4 +1,5 @@
 { pkgs ? import (builtins.fetchTarball {
+    name = "nixos-23.11-release";
     url = "https://github.com/NixOS/nixpkgs/archive/057f9aecfb71c4437d2b27d3323df7f93c010b7e.tar.gz";
     sha256 = "1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
   }) {}
