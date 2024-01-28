6cdd88265861e4211e6c80bdcff229707dfc4c96
Christian Lewe <ch_lewe@agdsn.me>
2024-01-28 13:33:58 +0100

    CSJ: Add CLI

--- csj.py
+++ csj.py
@@ -1,8 +1,12 @@
+import argparse
 import os
 import unittest
-from typing import Optional
+from datetime import date
 
+import rank
 from occurrence import OccurrenceBag, OccurrenceReader
+from rank import Rank
+
 
 def read_suw_bag(zip_dir_path: str) -> OccurrenceBag:
     zip_path = os.path.join(zip_dir_path, "CSJ_frequencylist_suw_ver201803.zip")
@@ -16,11 +20,36 @@ def read_suw_bag(zip_dir_path: str) -> OccurrenceBag:
         .with_count_index(6) \
         .read()
 
-def read_bag(zip_dir_path: str) -> OccurrenceBag:
-    return read_suw_bag(zip_dir_path)
-
 
 class TestCSJ(unittest.TestCase):
     def test_read_suw(self):
         bag = read_suw_bag("../data")
         self.assertEqual(41488, len(bag))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate CSJ frequency dictionary for Yomichan")
+
+    parser.add_argument("path_in", type=str, help="Path to directory with CSJ zip file")
+    parser.add_argument("path_out", type=str, help="Path of output dictionary")
+    parser.add_argument("--max", type=int, default=80000, help="Maximum term frequency included in dictionary")
+
+    args = parser.parse_args()
+
+    bag = read_suw_bag(args.path_in)
+    it = rank.from_counts(bag.to_counts())
+    it = rank.below_max_rank(it, args.max)
+
+    Rank.dictionary(list(it)) \
+        .with_title("話し言葉") \
+        .with_revision(f"data v2018-03 yomi v{date.today().isoformat()} SUW") \
+        .with_author("NINJAL, uncomputable") \
+        .with_url("https://github.com/uncomputable/japanese-tools") \
+        .with_description("""『日本語話し言葉コーパス（CSJ）』は、日本語の自発音声を大量にあつめて多くの研究用情報を付加した話し言葉研究用のデータベースであり、国立国語研究所・ 情報通信研究機構（旧通信総合研究所）・ 東京工業大学 が共同開発した、質・量ともに世界最高水準の話し言葉データベースです。
+
+        https://clrd.ninjal.ac.jp/csj/index.html""") \
+        .with_attribution("CC BY-NC-ND 3.0 https://creativecommons.org/licenses/by-nc-nd/3.0/deed.ja") \
+        .writer() \
+        .with_path(args.path_out) \
+        .in_chunks(10000) \
+        .write()
